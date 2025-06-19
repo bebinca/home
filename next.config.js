8a3358764f6a673ffb530415be13b01100302d23
bebinca <bebincaa@163.com>
2025-06-19 21:49:44 +0800

    merge

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    output: 'export', // 生成静态文件
+    basePath: '/home', // 改成你的 GitHub 仓库名（不是用户名）
+    images: {
+      unoptimized: true, // 如果你用了 <Image>，需要禁用图像优化
+    },
+  }
+  
+  module.exports = nextConfig
